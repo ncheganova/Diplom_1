18e59c69b256b30ef22ce21918b6effd1afb1ccc
Natalia Cheganova <ncheganova@yandex.ru>
2025-11-17 21:53:59 +0700

    solving warnings

--- src/test/java/data/DataModel.java
+++ src/test/java/data/DataModel.java
@@ -1,8 +1,5 @@
 package data;
 
-import praktikum.Bun;
-import praktikum.Ingredient;
-
 public class DataModel {
     public static final float PRICE_OF_BUN_1 = 100;
     public static final float PRICE_OF_BUN_2 = 200;
